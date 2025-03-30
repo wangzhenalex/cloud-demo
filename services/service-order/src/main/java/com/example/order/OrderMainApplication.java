78715c81665a8fd461c2cb07cab32b5b45939e8a
zhenwang <wangzhen_1990@163.com>
2025-03-30 08:55:49 +0800

    nacos

--- services/service-order/src/main/java/com/example/order/OrderMainApplication.java
+++ services/service-order/src/main/java/com/example/order/OrderMainApplication.java
@@ -1,8 +1,16 @@
 package com.example.order;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * @author zhenwang
@@ -15,5 +23,23 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class OrderMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager manager){
+        return args -> {
+            ConfigService configService = manager.getConfigService();
+            configService.addListener("service-order.properties", "DEFAULT_GROUP", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return Executors.newFixedThreadPool(4);
+                }
+
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    System.out.println("configInfo = " + configInfo);
+                }
+            });
+        };
     }
 }
--- services/service-order/src/main/java/com/example/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/example/order/controller/OrderController.java
@@ -1,7 +1,10 @@
 package com.example.order.controller;
 
+import com.example.order.properties.OrderProperties;
 import com.example.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +16,32 @@ import com.example.order.Order;
  * @time 2025/3/29 16:57
  * @description
  **/
+@RefreshScope//自动刷新
 @RestController
 public class OrderController {
 
     @Autowired
     OrderService orderService;
 
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @Autowired
+    OrderProperties orderProperties;
+
+    @GetMapping("/config")
+    public String config(){
+        return "order.timeout="+orderProperties.getTimeout()+"； " +
+                "order.auto-confirm="+orderProperties.getAutoConfirm() +"；"+
+                "order.db-url="+orderProperties.getDbUrl();
+    }
+    @GetMapping("/config1")
+    public String config1(){
+        return "order.timeout="+orderTimeout+"； " +
+                "order.auto-confirm="+orderAutoConfirm +"；";
+    }
     @GetMapping("/create")
     public Order createOrder(@RequestParam("userId") Long userId,
                              @RequestParam("productId") Long productId) {
